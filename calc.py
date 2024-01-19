74826266fec96f6fb25c9923afa4194526a7a2ae
evandropdeo <evandropin@gmail.com>
2024-01-19 10:59:12 -0300

    Create calc.py
    Calculadora das 4 operações em Python

---
+++ calc.py
@@ -0,0 +1,40 @@
+def value():    
+    return float(input("Numero: "))
+    
+def operator():
+    valid_ope = ["+","-","*","/","="]
+    ope = input("Operação: [+, -, *, / ou =]")
+    if ope not in valid_ope:
+        print("Operação inválida!")        
+    return ope
+
+def calc(value1, operator, value2):
+    res = 0.0
+    if operator == "+":
+        res = value1 + value2
+    elif operator == "-":
+        res = value1 - value2
+    elif operator == "*":
+        res = value1 * value2
+    elif operator == "/":
+        res = value1 / value2
+    else:
+        print("Operação inválida!")
+    return res
+
+def calculator():
+    dic ={}
+    while True:
+        new_value = value()
+        ope = operator()
+        res = calc(dic["value1"], dic["operator"], new_value) if len(dic) > 1 else new_value 
+
+        if ope == "=":
+            print("Resultado: ", res)
+            break
+        else:
+            dic["value1"] = res
+            dic["operator"] = ope
+            
+if _name_ == "_main_":
+    calculator()
